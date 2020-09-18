dc64e4d82d384dc31c7a6c165d14f7a6b287d1d7
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2020-09-18 09:24:41 +0530

    Update run.py

--- run.py
+++ run.py
@@ -3,3 +3,10 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 from sklearn.preprocessing import MinMaxScaler
+# %%
+rating = pd.read_csv('data/BX-Book-Ratings.csv', sep=';', error_bad_lines=False, encoding="latin-1")
+user = pd.read_csv('data/BX-Users.csv', sep=';', error_bad_lines=False, encoding="latin-1")
+book = pd.read_csv('data/BX-Books.csv', sep=';', error_bad_lines=False, encoding="latin-1")
+book_rating = pd.merge(rating, book, on='ISBN')
+cols = ['Year-Of-Publication', 'Publisher', 'Book-Author', 'Image-URL-S', 'Image-URL-M', 'Image-URL-L']
+book_rating.drop(cols, axis=1, inplace=True)
