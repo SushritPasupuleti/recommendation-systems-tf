e4be315cbe7873037deb9b9f5144a38d5e0c4db5
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2020-09-18 09:32:27 +0530

    Update run.py

--- run.py
+++ run.py
@@ -86,3 +86,18 @@ biases = {
     'decoder_b1': tf.Variable(tf.random_normal([num_hidden_1], dtype=tf.float64)),
     'decoder_b2': tf.Variable(tf.random_normal([num_input], dtype=tf.float64)),
 }
+# %%
+def encoder(x):
+    layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['encoder_h1']), biases['encoder_b1']))
+    layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, weights['encoder_h2']), biases['encoder_b2']))
+    return layer_2
+
+def decoder(x):
+    layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['decoder_h1']), biases['decoder_b1']))
+    layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, weights['decoder_h2']), biases['decoder_b2']))
+    return layer_2
+# %%
+encoder_op = encoder(X)
+decoder_op = decoder(encoder_op)
+y_pred = decoder_op
+y_true = X
